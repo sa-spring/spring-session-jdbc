1341c61921becd982f09eb6647b5271eb6b409e3
Chun <caochun@gmail.com>
2020-04-11 19:23:32 +0800

    fix

--- src/main/java/com/example/springsessionjdbc/controller/SpringSessionJdbcController.java
+++ src/main/java/com/example/springsessionjdbc/controller/SpringSessionJdbcController.java
@@ -7,7 +7,6 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
